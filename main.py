72d9d16b803933872f2ae9414d4b3521f85f7238
aBAY1999 <ibrahimabay123@gmail.com>
2020-10-09 00:21:36 +0300

    commit via pycharm

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+hasan: str = 'aliihsan'
+ali = ['34', 'allah', 'muhammed2']
+int(ali[0])
+print(hasan)
+huseyin = '5'
+print(huseyin)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(hasan[0:18])
+# ali[0] +=1
+ali[1] += '1'
+print(ali[:])
+4 + 5
+if (18 < 5):
+    print('18>5')
+elif 1:
+    print('elif')
+else:
+    print('rrrrrr')
+i=5
+while i<10:
+    print(i)
+    i+=1
+    if i>7:
+        break
 
+i = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(10):
+    if  i == 5:
+        while 1:
+            continue
+    print(i)
