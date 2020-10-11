b1abbdd80e91f9f106f38cd302472b717c9465fd
aBAY1999 <ibrahimabay123@gmail.com>
2020-10-11 20:49:33 +0300

    by me5

--- pygame-demo.py
+++ pygame-demo.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-display_width = 800             #ekran boyutu
-display_height = 500
+display_width = 825             #ekran boyutu
+display_height = 525
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Pacman')
@@ -17,13 +17,11 @@ kapaliImg = pygame.image.load('res/kapali.png')
 sag_ortaImg = pygame.image.load('res/sag_orta.png')
 sag_acikImg = pygame.image.load('res/sag_acik.png')
 
-
 def car(img, x, y):
     gameDisplay.blit(img, (x, y))
 
-
-x = (display_width * 0.5-12)
-y = (display_height * 0.5-12)
+x = (display_width * 0.5)
+y = (display_height * 0.5)
 i=0
 x_change = 0
 y_change = 0
@@ -33,30 +31,35 @@ while not crashed:
 
         if event.type == pygame.QUIT:
             crashed = True
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN :
+            x_change = 0
+            y_change = 0
             if event.key == pygame.K_LEFT:
                 x_change = - 3
-                y_change = 0
             elif event.key == pygame.K_RIGHT:
                 x_change = 3
-                y_change = 0
             elif event.key == pygame.K_UP:
                 y_change = - 3
-                x_change = 0
             elif event.key == pygame.K_DOWN:
                 y_change = 3
-                x_change = 0
+
     x += x_change
-    y+= y_change
+    y += y_change
+    if x>750 :x= 750
+    if x<50: x=50
+    if y > 450:y=450
+    if 50 > y :y=50
 
     gameDisplay.fill(white)
-    if i % 18 < 6:
+    if  i % 18 < 6:
+        if i==18:i=0 # i nin degerinin fazla buyuyup ram tutmamasi icin
         car(kapaliImg, x, y)
     elif i % 18 < 12:
         car(sag_ortaImg, x, y)
     else:
         car(sag_acikImg, x, y)
     i+=1
+
     pygame.display.update()
 
     clock.tick(60)
