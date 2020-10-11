6e75ccc3b657782beed1fec46a85fd715466ed54
Ali İhsan Şenel <aliihsansenel0@gmail.com>
2020-10-11 16:57:58 +0300

    commit via pycharm by ihsan

--- pygame-demo.py
+++ pygame-demo.py
@@ -18,8 +18,9 @@ sag_ortaImg = pygame.image.load('res/sag_orta.png')
 sag_acikImg = pygame.image.load('res/sag_acik.png')
 
 
-def car(img, x, y):
-    gameDisplay.blit(img, (x, y))
+def drawImg(img, x, y):
+    rImg = pygame.transform.rotate(img, 90 * yon)
+    gameDisplay.blit(rImg, (x, y))
 
 
 x = (display_width * 0.5-12)
@@ -27,6 +28,8 @@ y = (display_height * 0.5-12)
 i=0
 x_change = 0
 y_change = 0
+yon = 0;
+
 while not crashed:
 
     for event in pygame.event.get():
@@ -35,27 +38,31 @@ while not crashed:
             crashed = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
+                yon = 2;
                 x_change = - 3
                 y_change = 0
             elif event.key == pygame.K_RIGHT:
+                yon = 0;
                 x_change = 3
                 y_change = 0
             elif event.key == pygame.K_UP:
+                yon = 1;
                 y_change = - 3
                 x_change = 0
             elif event.key == pygame.K_DOWN:
+                yon = 3;
                 y_change = 3
                 x_change = 0
     x += x_change
     y+= y_change
 
     gameDisplay.fill(white)
-    if i % 18 < 6:
-        car(kapaliImg, x, y)
-    elif i % 18 < 12:
-        car(sag_ortaImg, x, y)
+    if i % 27 < 9:
+        drawImg(kapaliImg, x, y)
+    elif i % 27 < 18:
+        drawImg(sag_ortaImg, x, y)
     else:
-        car(sag_acikImg, x, y)
+        drawImg(sag_acikImg, x, y)
     i+=1
     pygame.display.update()
 
